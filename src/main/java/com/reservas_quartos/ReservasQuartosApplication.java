bce53b90de5c6c82e69d7b86de1acfc6fbf8fedd
maria <dilmasoares649@gmail.com>
2025-10-21 16:11:17 -0300

    primeiro commit estrutura inicial

---
+++ src/main/java/com/reservas_quartos/ReservasQuartosApplication.java
@@ -0,0 +1,13 @@
+package com.reservas_quartos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasQuartosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasQuartosApplication.class, args);
+	}
+
+}
